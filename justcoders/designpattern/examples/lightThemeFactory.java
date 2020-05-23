4f7cf45712e5ad8a1c96aa20aaa1c10456b02987
sathya narayanan <mpsathya93@gmail.com>
2020-05-23 17:01:12 +0530

    Create lightThemeFactory.java

---
+++ justcoders/designpattern/examples/lightThemeFactory.java
@@ -0,0 +1,15 @@
+package AbstractDesignPattern;
+
+public class LightThemeFactory implements ThemeFactory {
+
+	@Override
+	public BackgroundThemeColor createBackgroundThemeColor() {
+		return new LightThemeBackground();
+	}
+
+	@Override
+	public Text createText(String text) {
+		return new LightThemeText(text);
+	}
+
+}
