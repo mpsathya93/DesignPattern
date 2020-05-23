f1d1e9a4d4e3dda5e4c41bed28aa2a3cd00fa3b5
sathya narayanan <mpsathya93@gmail.com>
2020-05-23 17:05:40 +0530

    Create DarkThemeText.java

---
+++ justcoders/designpattern/examples/DarkThemeText.java
@@ -0,0 +1,21 @@
+package AbstractDesignPattern;
+
+public class DarkThemeText implements Text{
+
+	private String text;
+	
+	public DarkThemeText(String text)
+	{
+		this.text=text;
+	}
+	@Override
+	public String getcolor() {
+		return "White";
+	}
+
+	@Override
+	public String getText() {
+		return text;
+	}
+
+}
